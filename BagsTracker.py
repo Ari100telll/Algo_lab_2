0e8da174a8011f260a12017051ee06ad592a7863
Nikita Horb <masternik007@gmail.com>
2020-10-31 12:31:16 +0200

    Add Unit Tests

--- BagsTracker.py
+++ BagsTracker.py
@@ -1,11 +1,8 @@
-if __name__ == '__main__':
-    n = 10  # int(input())
-    w = 3  # int(input())
-    h = 2  # int(input())
+def get_min_size_of_square(n, w, h):
     count_of_row = 2
     area = n * w
-    area_last = area + 1
-    while area_last > area:
+    area_last = area
+    while area_last >= area and count_of_row <= n:
         area_last = area
         if n % count_of_row != 0:
             count_in_column = round(n / count_of_row)
@@ -15,4 +12,12 @@ if __name__ == '__main__':
             count_in_column = n / count_of_row
         area = min(area, max(count_in_column * w, h * count_of_row))
         count_of_row += 1
-    print(area)
+    return area
+
+
+if __name__ == '__main__':
+    # print(get_min_size_of_square(10, 2, 3))
+    print(get_min_size_of_square(2, 100000000, 99999999))
+
+    # print(get_min_size_of_square(101, 1, 1))
+    # print(get_min_size_of_square(16, 12, 13))
