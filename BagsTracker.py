c932de2c2138c16ef19e6e136d30c263910a0a5f
Nikita Horb <masternik007@gmail.com>
2020-11-03 17:35:24 +0200

    Change to bin search. Split Unit tests.

--- BagsTracker.py
+++ BagsTracker.py
@@ -5,18 +5,22 @@ def get_min_size_of_square(n, w, h):
     left = 1
     right = max(w, h) * n
     while True:
-        if right - left == 1:
+        if right <= left:
             ans = right
             break
-        middle = ceil((right - left) / 2 + left)
+        middle = floor((right - left) / 2 + left)
         count_in_row = floor(middle / w)
         count_in_column = floor(middle / h)
-        area = count_in_row * count_in_column
-        if area < n:
-            left = middle
-        elif area > n:
+        count_of_rectangles = count_in_row * count_in_column
+        if count_of_rectangles < n:
+            left = middle + 1
+        elif count_of_rectangles > n:
             right = middle
         else:
             ans = middle
             break
     return ans
+
+
+if __name__ == '__main__':
+    print(get_min_size_of_square(10, 100000000, 99999999))
